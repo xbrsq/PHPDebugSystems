3a43cdda315d1dd8df551fe209fefbdc8cecc591
xbrsq <xbrsq2@gmail.com>
2023-09-26 11:09:51 -0400

    added base of send
    not fully working yet.

---
+++ send.js
@@ -0,0 +1,38 @@
+
+
+function add_row(){
+    var table = document.getElementById("maintable");
+    var row = document.createElement("tr");
+    var key = document.createElement("td");
+    var val = document.createElement("td");
+    var ops = document.createElement("td");
+
+    var key_input = document.createElement("input");
+    val_input.id = "key_" + caIterator;
+    var val_input = document.createElement("input");
+    val_input.id = "val_" + caIterator;
+    var ops_input = document.createElement("button");
+    ops_input = del_parent;
+    ops_input.id = "del_" + caIterator++;
+    ops_input.value = false;
+    var ops_text = document.createTextNode(" Delete ")
+
+    key.appendChild(key_input);
+    val.appendChild(val_input);
+    ops.appendChild(ops_input);
+    ops.appendChild(ops_text);
+
+    row.appendChild(key);
+    row.appendChild(val);
+    row.appendChild(ops);
+
+    table.appendChild(row);
+}
+
+function del_parent(){
+    this.parentElement.parentElement.remove();
+}
+
+function set_vals(){
+
+}
